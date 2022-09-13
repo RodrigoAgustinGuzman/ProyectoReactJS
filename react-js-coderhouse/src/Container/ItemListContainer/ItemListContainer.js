19119f098e00365b2bb9c5d159ae03dd0fea085b
Rodrigo Guzman <guzman.roo@hotmail.com>
2022-09-13 07:43:46 -0300

    Contador

--- react-js-coderhouse/src/Container/ItemListContainer/ItemListContainer.js
+++ react-js-coderhouse/src/Container/ItemListContainer/ItemListContainer.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import {ItemCount} from '../../components/header/ItemCount/index'
 
 const ItemListContainer = ({greeting}) => {
   return (
+    <>
     <h1>
       {greeting}
     </h1>
+    <ItemCount initial={1} stock={10} onAdd={() => {} } />
+    </>
   )
 }
 
---
+++ react-js-coderhouse/src/components/header/ItemCount/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useState } from 'react'
+
+const ItemCount = ({initial, stock, onAdd}) => {
+
+const [count, setCount]= useState(initial)
+
+const increase = () => count < stock && setCount(count + 1)
+const decrease = () => count > initial && setCount (count - 1)
+
+  return (
+    <div>
+      <button onClick={decrease} >-</button>
+      {count}
+      <button onClick={increase}>+</button>
+      <button>Agregar al carrito</button>
+    </div>
+  )
+}
+
+export {ItemCount}
